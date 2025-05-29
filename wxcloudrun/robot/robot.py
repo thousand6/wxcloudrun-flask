c3e4ea034d1e5983ddd09ee15226cf946931cafa
larryzhang <larryzhang@etekcity.com.cn>
2025-05-29 13:50:28 +0800

    media

--- wxcloudrun/robot/robot.py
+++ wxcloudrun/robot/robot.py
@@ -45,7 +45,7 @@ def text_to_image(text, font_path='Kaiti.ttf', font_size=20, image_path='output.
 
 def upload_image(image):
     url = 'http://api.weixin.qq.com/cgi-bin/media/upload'
-    files = {'media': (image)}
+    files = {'media': image}
     response = requests.post(url, params={'type':'image'}, files=files)
     return response.text
 
